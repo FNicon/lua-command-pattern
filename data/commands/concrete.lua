4fd314282f8579eb94a96d900e1e2553c9d15bb3
Finiko Kasula Novenda <mybeddrom@gmail.com>
2025-05-08 23:55:40 +0900

    add command concrete data

---
+++ data/commands/concrete.lua
@@ -0,0 +1,42 @@
+return {
+	downright = {
+		execute = {x = 1, y = 1},
+		undo = {x = -1, y = -1},
+		label = "↘"
+	},
+	upright = {
+		execute = {x = 1, y = -1},
+		undo = {x = -1, y = 1},
+		label = "↗"
+	},
+	upleft = {
+		execute = {x = -1, y = -1},
+		undo = {x = 1, y = 1},
+		label = "↖"
+	},
+	downleft = {
+		execute = {x = -1, y = 1},
+		undo = {x = 1, y = -1},
+		label = "↙"
+	},
+	right = {
+		execute = {x = 1, y = 0},
+		undo = {x = -1, y = 0},
+		label = "→"
+	},
+	left = {
+		execute = {x = -1, y = 0},
+		undo = {x = 1, y = 0},
+		label = "←"
+	},
+	down = {
+		execute = {x = 0, y = 1},
+		undo = {x = 0, y = -1},
+		label = "↓"
+	},
+	up = {
+		execute = {x = 0, y = -1},
+		undo = {x = 0, y = 1},
+		label = "↑"
+	},
+}
