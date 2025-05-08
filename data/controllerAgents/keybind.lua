80eec917b1499da9c78917161316be9bb159c16d
Finiko Kasula Novenda <mybeddrom@gmail.com>
2025-05-08 23:53:51 +0900

    add keybind data

---
+++ data/controllerAgents/keybind.lua
@@ -0,0 +1,14 @@
+return {
+	upArrow = "up",
+	downArrow = "down",
+	leftArow = "left",
+	rightArrow = "right",
+	upKey = "w",
+	downKey = "s",
+	leftKey = "a",
+	rightKey = "d",
+	cancel = "backspace",
+	redo = "r",
+	execute = "space",
+	undo = "u"
+}
